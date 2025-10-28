81dba681adc423206e2341465f700645aec32c79
ritikverma1628 <ritikisverma9000@gmail.com>
2025-10-28 19:02:02 +0530

    Restructuring reviews using express.Router()

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const Review = require('./models/reviews');
 const path = require("path")
 const methodOverride = require("method-override")
-const asyncWrap = require("./utils/asyncWrap");
 const ejsMate = require('ejs-mate');
-const Listing = require("./models/listings/listings")
-const ExpressError = require("./utils/expressError")
-const {listingValidations,reviewValidations} = require('./joiValidations')
-const listingsRoutes = require('./routes/listings')
+const ExpressError = require("./utils/expressError");
+const listingsRoutes = require('./routes/listings');
+const reviewRoutes = require('./routes/review')
 const app = express();
+
+app.engine("ejs",ejsMate)
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
+app.use('/listings/:id/reviews',reviewRoutes);
+app.use('/listings',listingsRoutes);
 
 
 async function main(){
@@ -22,60 +29,14 @@ main().then(()=>{
 });
 
 
-app.engine("ejs",ejsMate)
-app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"))
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(methodOverride('_method'));
-app.use('/listings',listingsRoutes);
-
-
-
-
-//server-side validations for review form
-const validateReview = (req,res,next)=>{
-    const {error} = reviewValidations.validate(req.body)
-    if(error){
-        throw new ExpressError(404, error)
-    }
-    else{
-        next();
-    }
-}
-
 app.listen(3000,()=>{
     console.log("Server listening");
 })
 
+
 app.get("/",(req,res)=>{
     res.send("Hello, this is home page");
 })
-
-
-
-//post route for review
-app.post('/listings/:id/reviews', validateReview, asyncWrap(async(req,res)=>{
-    let review = await Review.create(req.body.review)
-    const listing = await Listing.findById(req.params.id);
-    listing.reviews.push(review);
-    await listing.save();
-    res.redirect(`/listings/${req.params.id}`)
-}))
-
-//deleting the review
-app.delete("/listings/:id/reviews/:reviewId", asyncWrap(async(req,res)=>{
-    const listingId = req.params.id;
-    const reviewId = req.params.reviewId;
-
-    await Review.deleteOne({_id:reviewId});
-    await Listing.findOneAndUpdate({_id:listingId}, {$pull:{reviews:reviewId}});
-
-    res.redirect(`/listings/${listingId}`);
-}))
-
-
 
 app.use("/",(req,res,next)=>{
     next(new ExpressError(404, 'Page not found!'))
---
+++ routes/review.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router({mergeParams:true});
+const Review = require('../models/reviews');
+const Listing = require("../models/listings/listings")
+const asyncWrap = require("../utils/asyncWrap");
+const ExpressError = require("../utils/expressError")
+const {reviewValidations} = require('../joiValidations')
+
+
+
+const validateReview = (req,res,next)=>{
+    const {error} = reviewValidations.validate(req.body)
+    if(error){
+        throw new ExpressError(404, error)
+    }
+    else{
+        next();
+    }
+}
+
+router.post('/', validateReview, asyncWrap(async(req,res)=>{
+    let review = await Review.create(req.body.review)
+    const listing = await Listing.findById(req.params.id);
+    listing.reviews.push(review);
+    await listing.save();
+    res.redirect(`/listings/${req.params.id}`)
+}))
+
+//deleting the review
+router.delete("/:reviewId", asyncWrap(async(req,res)=>{
+    const listingId = req.params.id;
+    const reviewId = req.params.reviewId;
+
+    await Review.deleteOne({_id:reviewId});
+    await Listing.findOneAndUpdate({_id:listingId}, {$pull:{reviews:reviewId}});
+
+    res.redirect(`/listings/${listingId}`);
+}))
+
+
+module.exports = router;
