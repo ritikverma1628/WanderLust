1651469f7663319a0570aed5797306adc4d4b374
ritikverma1628 <ritikisverma9000@gmail.com>
2025-11-06 19:23:04 +0530

    Compacting listing routes using express.Router().route()

--- routes/listings.js
+++ routes/listings.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const {isLoggedIn , isOwner, validateListing} = require('../middleware');
 const listingsController = require('../controller/listings')
 
-
-router.get("/", listingsController.getListings)
+router
+    .route('/')
+    .get( listingsController.getListings)
+    .post( validateListing, listingsController.postListing)
 
 router.get("/new",isLoggedIn, listingsController.renderNewForm)
-router.post("/",validateListing, listingsController.postListing)
 
-router.delete("/:id" , isLoggedIn , isOwner , listingsController.destroyListing)
+router
+    .route('/:id')
+    .delete( isLoggedIn , isOwner , listingsController.destroyListing)
+    .patch( validateListing , isOwner , listingsController.editListing)
+    .get( listingsController.showListing)
 
 
 router.get("/:id/edit" , isLoggedIn , isOwner , listingsController.renderEditForm);
-router.patch("/:id" , validateListing , isOwner , listingsController.editListing)
-
-
-router.get("/:id", listingsController.showListing)
-
 
 module.exports = router;
