188b0577609f1a75802ab54adc96fc9e293ed360
ritikverma1628 <ritikisverma9000@gmail.com>
2025-11-23 20:15:11 +0530

    customising the defualt mapbo marker

--- public/js/map.js
+++ public/js/map.js
@@ -4,5 +4,10 @@ mapboxgl.accessToken = mapToken;
         center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+    
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+    new mapboxgl.Marker(el)
+    .setLngLat(coordinates)
+    .addTo(map);
